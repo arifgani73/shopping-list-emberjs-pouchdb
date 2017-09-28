932cc233a80c1845ddec1a65c4df6b9b689db828
vabarbosa <va@us.ibm.com>
2017-09-28 12:14:01 -0400

    demo

--- app/components/shopping-list-settings-modal.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember'
-
-export default Ember.Component.extend({
-  pouchService : Ember.inject.service(),
-
-  settings: Ember.computed('pouchService.settings', function () {
-    return this.get('pouchService.settings')
-  })
-})
